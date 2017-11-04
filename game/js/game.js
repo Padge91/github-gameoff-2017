f82c66a4d2d3d8524d094fe2578f1809ff304457
Nick <nickpadge@gmail.com>
2017-11-04 13:48:31 -0400

    Added characters and constant lighting effects to the arcade

--- game/js/game.js
+++ game/js/game.js
@@ -12,10 +12,10 @@ function preload() {
 function loadAnimations(){
 	var animationsPath="/images/";
 	var extension=".png";
-	var animations=["arcade-screen"];
+	var animations=[{name:"arcade-screen", x:320,y:270,size:2}];
 	
 	for (var i = 0; i < animations.length; i++){
-		game.load.spritesheet(animations[i], animationsPath+animations[i]+extension, 320, 270, 2);
+		game.load.spritesheet(animations[i].name, animationsPath+animations[i].name+extension, animations[i].x, animations[i].y, animations[i].size);
 	}
 }
 
@@ -23,7 +23,7 @@ function loadAnimations(){
 function loadTextures(){
 	var texturesPath="/images/";
 	var extension=".png";
-	var textures=["test", "floor-pattern", "arcade-machine-side-perspective-red", "arcade-machine-side-perspective-purple", "arcade-machine-side-perspective-green", "arcade-machine-side-perspective-blue", "arcade-machine-front-pixelated"];
+	var textures=["test", "floor-pattern", "arcade-machine-side-perspective-red", "arcade-machine-side-perspective-purple", "arcade-machine-side-perspective-green", "arcade-machine-side-perspective-blue", "arcade-machine-front-pixelated", "character1","character2","character3","character4","character1-back","character2-back","lighting"];
 	
 	for (var i = 0; i < textures.length; i++){
 		game.load.image(textures[i], texturesPath+textures[i]+extension);
@@ -76,7 +76,7 @@ function loadArcadeMachines(){
 	}
 
 	var flashingScaleX = .4, flashingScaleY=.7;
-	var flashingAnimationLocations = [{x:110,y:150,rotation:0, speed:10},{x:110,y:250,rotation:0, speed:0},{x:110,y:350,rotation:0, speed:2},{x:580,y:350,rotation:0, speed:0},{x:580,y:250,rotation:0, speed:2},{x:380,y:350,rotation:180, speed:10},{x:380,y:250,rotation:180, speed:1},{x:900,y:150,rotation:90, speed:1},{x:400,y:150,rotation:90, speed:0},{x:800,y:150,rotation:90, speed:10}];
+	var flashingAnimationLocations = [{x:110,y:150,rotation:0, speed:5},{x:110,y:250,rotation:0, speed:0},{x:110,y:350,rotation:0, speed:0},{x:580,y:350,rotation:0, speed:5},{x:580,y:250,rotation:0, speed:0},{x:380,y:350,rotation:180, speed:5},{x:380,y:250,rotation:180, speed:0},{x:900,y:150,rotation:90, speed:0},{x:400,y:150,rotation:90, speed:0},{x:800,y:150,rotation:90, speed:5}];
 	var animationName = "arcade-screen";
 	for (var i  = 0; i < flashingAnimationLocations.length; i++){
 		var s = game.add.sprite(flashingAnimationLocations[i].x,flashingAnimationLocations[i].y, animationName);
@@ -94,7 +94,18 @@ function loadArcadeMachines(){
 
 //load characters
 function loadNPCs(){
+	var characters = [{x:90,y:400,sprite:"character1",reverse:false},{x:90,y:300,sprite:"character4",reverse:false},{x:300,y:300,reverse:true,sprite:"character3"},{x:550,y:300,reverse:false,sprite:"character2"},{x:740,y:100,reverse:false,sprite:"character1-back"},{x:240,y:100,reverse:false,sprite:"character2-back"}]
+	var scale = .3;
 
+	for (var i = 0; i < characters.length; i++){
+		var s = game.add.sprite(characters[i].x, characters[i].y, characters[i].sprite);
+		s.scale.setTo(scale, scale);
+		if (characters[i].reverse){
+                        s.anchor.setTo(.75,0);
+                        s.scale.x *= -1;
+                }
+	}
+	
 }
 
 //load arcade floor
@@ -113,4 +124,6 @@ function loadArcadeFloor(){
                         s.scale.setTo(scaleValue, scaleValue);
                 }
         }
+
+	game.add.sprite(0,0,"lighting");
 }
